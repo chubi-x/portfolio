3816c95c72303999ebe003965cddb377151a385a
chubi-x <chubixax@protonmail.com>
2022-08-18 10:13:16 +0100

    feat: add footer

--- src/App.js
+++ src/App.js
@@ -4,29 +4,32 @@ function App() {
   return (
     <div className="App">
       <div className="under-construction">
-        <lottie-player
-          src="https://assets6.lottiefiles.com/packages/lf20_AQcLsD.json"
-          background="transparent"
-          speed="1"
-          style={{ width: "280px", height: "280px" }}
-          loop
-          autoplay
-        ></lottie-player>
+        <div className="animation">
+          <lottie-player
+            src="https://assets6.lottiefiles.com/packages/lf20_AQcLsD.json"
+            background="transparent"
+            speed="1"
+            style={{ width: "100%", height: "280px" }}
+            loop
+            autoplay
+          ></lottie-player>
+        </div>
         <Ztext
           depth="0.7rem"
           direction="both"
           event="pointer"
-          eventRotation="65deg"
+          eventRotation="30deg"
           eventDirection="default"
           fade={false}
           layers={10}
           perspective="500px"
-          style={{
-            fontSize: "1.2rem",
-          }}
+          className="z-text"
         >
           <h1>Coming Soon 👀</h1>
         </Ztext>
+        <footer className="footer">
+          <a href="mailto:chubixax@protonmail.com">Email me</a>
+        </footer>
       </div>
     </div>
   );

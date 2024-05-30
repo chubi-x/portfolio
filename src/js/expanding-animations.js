34d2fe389f6c6030d5fe2907e14e66732ee0c473
chubi-x <chubixax@protonmail.com>
2024-05-30 21:02:28 +0100

    make index mobile responsive

--- src/js/expanding-animations.js
+++ src/js/expanding-animations.js
@@ -1,3 +1,4 @@
+import gsap from 'gsap'
 import { Flip } from 'gsap/Flip'
 class ExpandingAnimation {
   constructor (el) {
@@ -64,24 +65,22 @@ export class ExpandImageEffect2 extends ExpandingAnimation {
     const flipstate = Flip.getState(this.image)
     this.wrapElement.classList.remove('text--open')
 
-    Flip.to(flipstate, {
-      ease: 'sine',
-      simple: true,
+    const tl = gsap.timeline({
       scrollTrigger: {
         trigger: this.wrapElement,
-        start: 'center bottom',
+        start: 'clamp(center bottom)',
         end: 'center top',
         scrub: true
       }
-    }).to(this.expandTexts, {
+    })
+
+    const flip = Flip.to(flipstate, {
       ease: 'sine',
-      skewX: -20,
-      scrollTrigger: {
-        trigger: this.wrapElement,
-        start: 'center bottom',
-        end: 'center top',
-        scrub: true
-      }
+      simple: true
+    })
+    tl.add(flip).to(this.expandTexts, {
+      ease: 'sine',
+      skewX: -20
     })
   }
 }
@@ -98,8 +97,8 @@ export class ExpandImageEffect3 extends ExpandingAnimation {
       x: 0,
       scrollTrigger: {
         trigger: this.wrapElement,
-        start: 'center bottom',
-        end: 'center top',
+        start: 'clamp(center bottom)',
+        end: 'clamp(center top)',
         scrub: true
       }
     }).fromTo(
@@ -110,7 +109,7 @@ export class ExpandImageEffect3 extends ExpandingAnimation {
       },
       {
         ease: 'sine.inOut',
-        yPercent: -40,
+        yPercent: -20,
         skewX: -2,
         lineHeight: 2,
         opacity: 0.2,

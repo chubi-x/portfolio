c12d290a3efc83c2cd5810434259c99fba853608
chubi-x <chubixax@protonmail.com>
2025-04-12 22:08:55 +0100

    statically render the site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import node from "@astrojs/node";
-
-import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "server",
-  adapter: netlify()
+  output: "static",
 });
